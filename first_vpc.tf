0241f98ccbefa6f81bb7358510de65291ef1f1ec
mentalhetal <khz0825@naver.com>
2025-04-04 03:44:16 +0000

    VPC 2개 로드 밸런서 1개

---
+++ first_vpc.tf
@@ -0,0 +1,83 @@
+resource "aws_vpc" "my" {
+  cidr_block = "20.40.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MY-VPC"
+  }
+}
+
+# Public Subnet 생성
+resource "aws_subnet" "my" {
+  vpc_id                  = aws_vpc.my.id
+  cidr_block              = "20.40.1.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MY-Public-SN"
+  }
+}
+
+# Internet Gateway 생성
+resource "aws_internet_gateway" "my" {
+  vpc_id = aws_vpc.my.id
+  tags = {
+    Name = "MY-IGW"
+  }
+}
+
+# Public Route Table 생성
+resource "aws_route_table" "my" {
+  vpc_id = aws_vpc.my.id
+  tags = {
+    Name = "MY-Public-RT"
+  }
+}
+
+# Public Route Table에 인터넷 게이트웨이 연결
+resource "aws_route" "internet1" {
+  route_table_id         = aws_route_table.my.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my.id
+}
+
+# Public Subnet에 Public Route Table 연결
+resource "aws_route_table_association" "my" {
+  subnet_id      = aws_subnet.my.id
+  route_table_id = aws_route_table.my.id
+}
+
+# Security Group for Public Subnet
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 인스턴스 (Public Subnet)
+resource "aws_instance" "my" {
+  ami           = "ami-070e986143a3041b6"  # 예시로 Amazon Linux 2 AMI (리전마다 다를 수 있음)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  key_name   = "my-keypair-01"
+  tags = {
+    Name = "MY-EC2"
+  }
+}
---
+++ second_vpc.tf
@@ -0,0 +1,196 @@
+terraform {
+  required_version = ">= 1.5.0"  # 최소 Terraform 버전 1.5.0 이상
+}
+
+provider "aws" { }
+
+# VPC 생성
+resource "aws_vpc" "main" {
+  cidr_block = "10.40.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "CH3-VPC"
+  }
+}
+
+# Public Subnet 생성
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.40.1.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "ELB-Public-SN1"
+  }
+}
+
+# Public Subnet 생성
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.40.2.0/24"
+  availability_zone       = "ap-northeast-2c"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "ELB-Public-SN2"
+  }
+}
+
+# Internet Gateway 생성
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "ELB-IGW"
+  }
+}
+
+# Public Route Table 생성
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "ELB-Public-RT"
+  }
+}
+
+# Public Route Table에 인터넷 게이트웨이 연결
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+# Public Subnet에 Public Route Table 연결
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# Public Subnet에 Public Route Table 연결
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+# Security Group for Public Subnet
+resource "aws_security_group" "public_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # SSH 접근을 위해 모든 IP 허용
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # SSH 접근을 위해 모든 IP 허용
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]  # SSH 접근을 위해 모든 IP 허용
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 인스턴스 (Public Subnet)
+resource "aws_instance" "public" {
+  ami           = "ami-070e986143a3041b6"  # 예시로 Amazon Linux 2 AMI (리전마다 다를 수 있음)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public1.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  key_name   = "Server_01"
+
+  user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd
+                systemctl enable httpd
+                
+                # IMDSv2를 사용하여 인스턴스 ID 가져오기
+                TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+                INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id)
+
+                # index.html 생성
+                echo "<html><body><h1>My EC2 Instance: $INSTANCE_ID</h1></body></html>" > /var/www/html/index.html
+
+                # index.html 생성
+                # echo "<html><body><h1>SERVER-1</h1></body></html>" > /var/www/html/index.html
+                EOF
+
+  tags = {
+    Name = "SERVER-1"
+  }
+}
+
+# EC2 인스턴스 (Public Subnet)
+resource "aws_instance" "public1" {
+  ami           = "ami-070e986143a3041b6"  # 예시로 Amazon Linux 2 AMI (리전마다 다를 수 있음)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public2.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  key_name   = "Server_02"
+
+  user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd
+                systemctl enable httpd
+                
+                # IMDSv2를 사용하여 인스턴스 ID 가져오기
+                TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+                INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id)
+
+                # index.html 생성
+                echo "<html><body><h1>My EC2 Instance: $INSTANCE_ID</h1></body></html>" > /var/www/html/index.html
+
+                # index.html 생성
+                # echo "<html><body><h1>SERVER-2</h1></body></html>" > /var/www/html/index.html
+                EOF
+
+  tags = {
+    Name = "SERVER-2"
+  }
+}
+
+# EC2 인스턴스 (Public Subnet)
+resource "aws_instance" "public2" {
+  ami           = "ami-070e986143a3041b6"  # 예시로 Amazon Linux 2 AMI (리전마다 다를 수 있음)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public2.id
+  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  key_name   = "Server_02"
+
+  user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd
+                systemctl enable httpd
+                
+                # IMDSv2를 사용하여 인스턴스 ID 가져오기
+                TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+                INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-id)
+
+                # index.html 생성
+                echo "<html><body><h1>My EC2 Instance: $INSTANCE_ID</h1></body></html>" > /var/www/html/index.html
+
+                # index.html 생성
+                # echo "<html><body><h1>SERVER-3</h1></body></html>" > /var/www/html/index.html
+                EOF
+
+  tags = {
+    Name = "SERVER-3"
+  }
+}
